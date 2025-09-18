3e9cb0ad35d42f81bdfb85d0f3a79ddefb8b0f37
Kevin-Vel <veletocopak@gmail.com>
2025-09-17 20:36:08 -0500

    Add files via upload

---
+++ src/Persona.java
@@ -0,0 +1,18 @@
+public abstract class Persona {
+    protected  String nombre;
+    protected  String apellido;
+
+    public Persona(String nombre, String apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+
+    public String getNombre(){return nombre;}
+    public String getApellido(){return apellido;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setApellido(String apellido) {this.apellido = apellido;}
+
+    public abstract String mostrarInfo();
+}
+
