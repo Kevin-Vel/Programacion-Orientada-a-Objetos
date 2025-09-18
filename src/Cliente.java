f0de08eb6a854e8eed71d9b300962ed5a9c94b37
Kevin-Vel <veletocopak@gmail.com>
2025-09-18 15:34:38 -0500

    Add files via upload

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,28 +1,30 @@
-public class Cliente {
-    private int idcli, dni;
-    private String nom, apell, contraseña;
+public class Cliente extends Persona {
+    private int idcli;
+    private int dni;
+    private String contraseña;
 
+    // Constructor
     public Cliente(int idclie, int dni, String nom, String apell) {
-        this.idcli=idclie;
-        this.nom=nom;
-        this.apell=apell;
-        this.dni=dni;
-        this.contraseña=contraseña;
+        super(nom, apell);
+        this.idcli = idclie;
+        this.dni = dni;
+        this.contraseña = "";
     }
 
-    public String getContraseña(){return contraseña;}
-    public String getNom() {return nom;}
-    public String getApell(){return apell;}
-    public int getIdclie(){return idcli;}
-    public int getDni(){return dni;}
+    public int getIdclie() { return idcli; }
+    public int getDni() { return dni; }
+    public String getContraseña() { return contraseña; }
 
-    public void setNom(String nom){this.nom=nom;}
-    public void setApell(String apell){this.apell=apell;}
-    public void setContraseña(String contraseña){this.contraseña=contraseña;}
+    public void setContraseña(String contraseña) { this.contraseña = contraseña; }
 
     @Override
-    public String toString(){
-        return idcli + " " + dni + "-" + nom + "-" + apell;
+    public String mostrarInfo() {
+        return "Cliente: " + idcli + " - DNI: " + dni + " - " + nombre + " " + apellido;
+    }
+
+    @Override
+    public String toString() {
+        return idcli + " " + dni + " - " + nombre + " - " + apellido;
     }
 }
 
--- src/Empleados.java
+++ src/Empleados.java
@@ -1,17 +1,17 @@
-public class Empleados {
-    private String nombre, apellido, cargo;
+public class Empleados extends Persona {
+    private String cargo;
 
-    public Empleados(String nombre, String apellido, String cargo){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.cargo=cargo;
+    public Empleados(String nombre, String apellido, String cargo) {
+        super(nombre, apellido);
+        this.cargo = cargo;
     }
 
-    public String getNombre(){return nombre;}
-    public String getApellido(){return apellido;}
-    public String getCargo(){return cargo;}
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
 
-    public void setNombre(String nombre){this.nombre=nombre;}
-    public void setApellido(String apellido){this.apellido=apellido;}
-    public void setCargo(String cargo){this.cargo=cargo;}
+    @Override
+    public String mostrarInfo() {
+        return "Empleado: " + nombre + " " + apellido + " - Cargo: " + cargo;
+    }
 }
+
