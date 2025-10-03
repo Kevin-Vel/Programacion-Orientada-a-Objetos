a6cc8bbb7bada2c492363769dd2eacf84207169c
Kevin-Vel <veletocopak@gmail.com>
2025-10-02 22:33:01 -0500

    Add files via upload

--- src/Interfazes.java
+++ src/Interfazes.java
@@ -1,6 +1,11 @@
 public class Interfazes {
     public interface PersonaInterface {
         void setNombre(String nombre);
+
         String getNombre();
+    }
+
+    public interface Autenticable {
+        boolean autenticar(String usuario, String contraseña);
     }
 }
