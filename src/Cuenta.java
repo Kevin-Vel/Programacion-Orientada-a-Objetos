6d24a9e110051395308ad3638dbfe57adfb49ee2
Kevin-Vel <veletocopak@gmail.com>
2025-10-04 23:11:40 -0500

    Add files via upload

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -3,7 +3,7 @@ import java.io.Serializable;
 public class Cuenta implements Serializable {
     private  static final   long serialVersionUID=1L;
     private int numCuenta;
-    private String tipCuenta;
+    private TipoCuenta tipCuenta;
     private double saldo;
     private Cliente cliente;
 
@@ -15,12 +15,12 @@ public class Cuenta implements Serializable {
     }
 
     public int getNumCuenta() {return numCuenta;}
-    public String getTipCuenta() {return tipCuenta;}
+    public TipoCuenta getTipCuenta() {return tipCuenta;}
     public double getSaldo() {return saldo;}
     public Cliente getCliente() {return cliente;}
 
     public void setNumCuenta(int numCuenta){this.numCuenta=numCuenta;}
-    public void setTipCuenta(String tipCuenta){this.tipCuenta=tipCuenta;}
+    public void setTipCuenta(TipoCuenta tipCuenta){this.tipCuenta=tipCuenta;}
     public void setSaldo(double saldo){this.saldo=saldo;}
     public void setCliente(Cliente cliente){this.cliente=cliente;}
 }
