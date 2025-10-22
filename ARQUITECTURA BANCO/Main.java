c4d84e915f70c115cd12136d6b6d4db7746bf853
Kevin-Vel <veletocopak@gmail.com>
2025-10-22 13:10:37 -0500

    Delete ARQUITECTURA BANCO directory

--- ARQUITECTURA BANCO/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SistemaBanco sistema = new SistemaBanco();
-        Menu menu = new Menu(sistema);
-        menu.setVisible(true);
-    }
-}
