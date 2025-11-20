60798bb8251ecd9f8c6c3e1a3af8b903e9a35511
Kevin-Vel <veletocopak@gmail.com>
2025-11-19 19:32:19 -0500

    Add files via upload

--- src/MODELO/Empleados.java
+++ src/MODELO/Empleados.java
@@ -2,26 +2,27 @@ package MODELO;
 
 import CONTROLADOR.Autenticable;
 
-public class Empleados extends Persona implements Autenticable{
-    private String cargo;
-    private String EDni;
+public class Empleados extends Persona implements Autenticable {
 
-    public Empleados(String nombre, String apellido, String Edni, String password, String cargo) {
+    private String cargo;
+    private String eDni;
+
+    public Empleados(String nombre, String apellido, String eDni, String password, String cargo) {
         super(nombre, apellido, password);
         this.cargo = cargo;
-        this.EDni = Edni;
+        this.eDni = eDni;
     }
-
 
     public String getCargo() { return cargo; }
     public void setCargo(String cargo) { this.cargo = cargo; }
 
-    public String getEDni(){return EDni;}
-    public void setEDni(String EDni){this.EDni=EDni;}
+    public String geteDni() { return eDni; }
+    public void seteDni(String eDni) { this.eDni = eDni; }
 
     @Override
-    public boolean autenticar(String usuario, String contraseña){
-        return nombre.equals(usuario)&& password.equals(contraseña);
+    public boolean autenticar(String usuario, String contraseña) {
+        // Usuario = EDNI
+        return eDni.equals(usuario) && password.equals(contraseña);
     }
 
     @Override
